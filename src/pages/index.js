e31d4617a239130d0f9c2e4ad4c0499061f52d83
Amol Tangade <amolfb@gmail.com>
2019-10-11 01:42:27 +0530

    added content

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,13 +9,27 @@ const IndexPage = () => (
     <SEO title="Home" />
     <Container>
       <Row>
-        <Col>
+        <Col sm={12} lg={12}>
           <div className="d-flex justify-center align-center h-100 main text-center">
             <div className="flex-column align-center justify-center">
-              <h5 className="text-white">
-                Love great music? We do too. Introducing
-              </h5>
-              <h1 className="font-black text-white">Melodify</h1>
+              <Row>
+                <Col lg={2}></Col>
+                <Col>
+                  <h3 className="text-white font-semibold">Melodify</h3>
+                  <h1 className="font-black text-white">
+                    You are what you listen to
+                  </h1>
+                  <h5 className="text-white font-semibold">
+                    We're here to help you create an amazing set of playlists!
+                    Those songs you played over the time are going to be used
+                    for creating these playlists. We analize your music habits,
+                    what songs you play the most, which artist are you listen to
+                    all the time and bassed on that data we create amazing
+                    playlists to melodify your soul!
+                  </h5>
+                </Col>
+                <Col lg={2}></Col>
+              </Row>
               <SpotifyBtn />
             </div>
           </div>
