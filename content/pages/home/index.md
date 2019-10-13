12340e517b5133dce671f8cbbdb0d692a27bdc87
pratik kakkad <pratikkakkad21@gmail.com>
2019-10-14 00:23:26 +0530

    lazy load

--- content/pages/home/index.md
+++ content/pages/home/index.md
@@ -2,4 +2,6 @@
 page: "home"
 
 header: "Listen Your Music"
+image: /static/img/home-bg.jpg
+
 ---
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,42 +1,66 @@
 import React from 'react'
 import { Container, Row, Col } from 'react-grid-system'
+import { graphql } from 'gatsby'
+import BackgroundImage from 'gatsby-background-image'
 import SEO from '../components/seo'
 import '../styles/index.scss'
 import SpotifyBtn from '../components/SpotifyBtn'
 
-const IndexPage = () => (
+const IndexPage = props => (
   <div>
     <SEO title="Home" />
-    <Container>
-      <Row>
-        <Col sm={12} lg={12}>
-          <div className="d-flex justify-center align-center h-100 main text-center">
-            <div className="flex-column align-center justify-center">
-              <Row>
-                <Col lg={2}></Col>
-                <Col>
-                  <h3 className="text-white font-semibold">Melodify</h3>
-                  <h1 className="font-black text-white">
-                    You are what you listen to
-                  </h1>
-                  <h5 className="text-white font-semibold">
-                    We're here to help you discover those amazing sets of
-                    playlists! Those songs you've been playing over the time are
-                    going to be used for creating these playlists. We analize
-                    your music habits, what songs you play the most, which
-                    artist are you listen to all the time and based on that data
-                    we create amazing playlists to melodify your soul!!
-                  </h5>
-                </Col>
-                <Col lg={2}></Col>
-              </Row>
-              <SpotifyBtn />
+    <BackgroundImage
+      fluid={props.data.markdownRemark.frontmatter.image.childImageSharp.fluid}
+    >
+      <Container fluid>
+        <Row>
+          <Col sm={12} lg={12}>
+            <div className="d-flex justify-center align-center main text-center">
+              <div className="flex-column align-center justify-center">
+                <Row>
+                  <Col lg={2}></Col>
+                  <Col>
+                    <h3 className="text-white font-semibold">Melodify</h3>
+                    <h1 className="font-black text-white">
+                      You are what you listen to
+                    </h1>
+                    <h5 className="text-white font-semibold">
+                      We're here to help you discover those amazing sets of
+                      playlists! Those songs you've been playing over the time
+                      are going to be used for creating these playlists. We
+                      analize your music habits, what songs you play the most,
+                      which artist are you listen to all the time and based on
+                      that data we create amazing playlists to melodify your
+                      soul!!
+                    </h5>
+                  </Col>
+                  <Col lg={2}></Col>
+                </Row>
+                <SpotifyBtn />
+              </div>
             </div>
-          </div>
-        </Col>
-      </Row>
-    </Container>
+          </Col>
+        </Row>
+      </Container>
+    </BackgroundImage>
   </div>
 )
 
 export default IndexPage
+
+export const query = graphql`
+  query {
+    markdownRemark(frontmatter: { page: { eq: "home" } }) {
+      frontmatter {
+        header
+        image {
+          childImageSharp {
+            fluid(maxWidth: 1024) {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
+      }
+    }
+  }
+`
